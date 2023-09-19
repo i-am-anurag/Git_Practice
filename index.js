dc3c11d5ab74a8577f059ca0957bde55d120932d
Anurag Pandey <107468188+i-am-anurag@users.noreply.github.com>
2023-09-19 10:51:43 +0000

    added some codes

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 console.log("Hello form node js");
 
-const num = 10;
-console.log(num);
-console.log(num-10);
+function findMaxConsecutiveOnes (nums) {
+    let largestCount = 0;
+    let currentCount = 0;
+    for (let i = 0; i<nums.length; i++) {
+        if (nums[i] === 0) {
+            currentCount = 0;
+        } else {
+            currentCount++;
+        };
+        if (currentCount > largestCount) largestCount = currentCount;
+    };
+    return largestCount;
+};
+
+let arr = [1,2,1,1,1,5,1,6,1,1,0];
+const result = findMaxConsecutiveOnes(arr);
+
+console.log(result);
