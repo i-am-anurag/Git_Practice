2aedd5aa430ce2433fe2b905c4a2e6d166792f10
Anurag Pandey <107468188+i-am-anurag@users.noreply.github.com>
2023-09-19 11:07:42 +0000

    added max function on test file

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
 console.log("This is For testing File");
+
+const findMax = (arr)=> {
+    let max = 0;
+    for(let num of arr) {
+        if(num>max)
+            max = num;
+    }
+
+    return max;
+}
+
+const arr = [10,50,90,60,180,2740,560,620,10,55,84,98];
+
+const max = findMax(arr);
+console.log(`The max number is: ${max}`);
