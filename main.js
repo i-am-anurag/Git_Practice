98bd46ad06852689bb6cd549508dfcd3834e6bbb
Anurag Pandey <107468188+i-am-anurag@users.noreply.github.com>
2023-09-29 05:37:03 +0000

    added max function in main file
    Signed-off-by: Anurag Pandey <107468188+i-am-anurag@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const {runningSum} = require('./array');
+const {findMax} = require('./test');
 console.log("This is main file");
 // find running sum sum of 1d array 
-const arr = [10,15,50,60,1,90];
+const arr = [10,15,50,60,1,90,70];
 const output = runningSum(arr);
 console.log("The output of runnig array sum is:",output);
+const max = findMax(arr);
+console.log("The max value in the array is:",max);
